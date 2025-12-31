3124f4747b24db5cafc81322066e14d571cf6063
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-31 11:04:06 +0200

    Create the user repository to handle database transactions

---
+++ backend/src/main/java/inc/sims/hustles/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package inc.sims.hustles.repositories;
+
+import inc.sims.hustles.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
