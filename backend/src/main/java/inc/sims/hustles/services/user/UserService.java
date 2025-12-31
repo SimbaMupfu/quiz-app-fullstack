13d69fe105e465602d3b9f70cf26b748263ab666
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-31 12:52:33 +0200

    Create abstractions for the UserService.

--- backend/src/main/java/inc/sims/hustles/services/user/UserService.java
+++ backend/src/main/java/inc/sims/hustles/services/user/UserService.java
@@ -1,4 +1,9 @@
 package inc.sims.hustles.services.user;
 
+import inc.sims.hustles.models.User;
+
 public interface UserService {
+
+    User createUser(User user);
+    boolean hasUserWithEmail(String email);
 }
