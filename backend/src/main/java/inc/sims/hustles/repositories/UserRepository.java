bd71120a4ee9d755af488bd786d578f61b41dcb7
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-31 12:36:32 +0200

    Add the missing methods in the user repository.

--- backend/src/main/java/inc/sims/hustles/repositories/UserRepository.java
+++ backend/src/main/java/inc/sims/hustles/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package inc.sims.hustles.repositories;
 
+import inc.sims.hustles.enums.UserRole;
 import inc.sims.hustles.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByRole(UserRole userRole);
+
+    User findFirstByEmail(String email);
 }
