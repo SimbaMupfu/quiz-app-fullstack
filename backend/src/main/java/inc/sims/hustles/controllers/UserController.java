54f9493744bfa26d6b8e8f48060acf925e4e1c4a
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-31 13:29:40 +0200

    Add endpoint for signing up user.

---
+++ backend/src/main/java/inc/sims/hustles/controllers/UserController.java
@@ -0,0 +1,31 @@
+package inc.sims.hustles.controllers;
+
+import inc.sims.hustles.models.User;
+import inc.sims.hustles.services.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/auth")
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> signUpUser(@RequestBody User user){
+        if(userService.hasUserWithEmail(user.getEmail())){
+            return new ResponseEntity<>("User already exists", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        User newUser = userService.createUser(user);
+        if(newUser == null){
+            return new ResponseEntity<>("User not created. Please try again.", HttpStatus.NOT_ACCEPTABLE);
+        }else{
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        }
+    }
+}
