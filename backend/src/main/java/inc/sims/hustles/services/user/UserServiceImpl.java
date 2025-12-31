dbf3ddaf6c1fc6da8ea20615634cbaad0870fc2c
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-31 12:30:52 +0200

    Add functionality to create the admin if one does not exist.

--- backend/src/main/java/inc/sims/hustles/services/user/UserServiceImpl.java
+++ backend/src/main/java/inc/sims/hustles/services/user/UserServiceImpl.java
@@ -1,7 +1,29 @@
 package inc.sims.hustles.services.user;
 
+import inc.sims.hustles.enums.UserRole;
+import inc.sims.hustles.models.User;
+import inc.sims.hustles.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    private void createAdminUser(){
+        User optionalUser = userRepository.findByRole(UserRole.ADMIN);
+
+        if(optionalUser == null){
+            User user = new User();
+            user.setName("Name");
+            user.setEmail("admin@hustles.com");
+            user.setPassword("password");
+            user.setRole(UserRole.ADMIN);
+            userRepository.save(user);
+        }
+    }
 }
