5e4e9dec023e62252b80f9c204bbc43e605d100c
SimbaMupfu <scmupfururirwa@gmail.com>
2026-01-02 19:06:49 +0200

    Create TestDTO

---
+++ backend/src/main/java/inc/sims/hustles/dto/TestDTO.java
@@ -0,0 +1,12 @@
+package inc.sims.hustles.dto;
+
+import lombok.Data;
+
+@Data
+public class TestDTO {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Long time;
+}
